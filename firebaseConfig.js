e9cc9e37a885cc0b2ac48589aa584413ff1f7e5d
arash12javadi <arash.javadi.nu2425.w24052061@gmail.com>
2024-12-16 12:01:05 +0000

    added my app credentials

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,30 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//  //
- 
-// };
-
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDWELdHJnpLqMWGIZoBiCiuOQFpZukAqQ4",
+  authDomain: "e-hotel-manager.firebaseapp.com",
+  projectId: "e-hotel-manager",
+  storageBucket: "e-hotel-manager.firebasestorage.app",
+  messagingSenderId: "301876089939",
+  appId: "1:301876089939:web:0c0e6182688f57041622a6",
+  measurementId: "G-9T07MC283C",
+};
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
 // Initialize Auth with AsyncStorage persistence
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-})
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 // Get Database instance
 const database = getDatabase(app);
 
