dfb25ab54f5930a9c7773515758fff478a89fd10
arash12javadi <arash.javadi.nu2425.w24052061@gmail.com>
2024-12-23 00:55:24 +0000

    update admin dashboard logics

--- src/components/AdminPanel/AdminDashboardScreen.tsx
+++ src/components/AdminPanel/AdminDashboardScreen.tsx
@@ -1,9 +1,39 @@
-import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Text,
+} from "react-native";
 import RoomComponent from "@/components/RoomManagement/RoomComponent";
 import HamburgerMenu from "@/components/HamburgerMenu";
+import useCheckUserRole from "@/components/CheckUserRole";
+import { useRouter } from "expo-router";
 
 export default function App() {
+  const router = useRouter();
+  const role = useCheckUserRole();
+
+  useEffect(() => {
+    if (role !== "loading" && role === "") {
+      router.replace("/Login");
+    }
+  }, [role, router]);
+
+  if (role === "loading") {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#fff" />
+        <Text style={styles.loadingText}>Checking user status...</Text>
+      </View>
+    );
+  }
+
+  if (role === "" || role === "staff") {
+    router.replace("/Dashboard");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <HamburgerMenu />
@@ -15,5 +45,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingText: {
+    color: "#fff",
+    marginTop: 10,
   },
 });
