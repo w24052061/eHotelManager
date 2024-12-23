c44a258400c2067591073890562acfa776d1ae39
arash12javadi <arash.javadi.nu2425.w24052061@gmail.com>
2024-12-23 01:35:07 +0000

    Update AdminDashboardComplaintsTab.tsx

--- app/(tabs)/AdminDashboardComplaintsTab.tsx
+++ app/(tabs)/AdminDashboardComplaintsTab.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import AdminComplaintManagement from "@/components/AdminPanel/AdminComplaintManagement";
-// or import AdminDashboardComplaints from "...";
 
 export default function AdminComplaintsTab() {
   return (
