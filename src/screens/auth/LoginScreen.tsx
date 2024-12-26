0b467d8666cb4ad36b39bcd3e633636aa70efe0f
arash12javadi <arash.javadi.nu2425.w24052061@gmail.com>
2024-12-26 11:18:32 +0000

    redirect to home after login

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,29 +1,35 @@
 // src/screens/auth/LoginScreen.tsx
-import React, { useState } from 'react';
-import { TextInput, TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@firebaseConfig'; // Import firebase configuration
-import { useRouter } from 'expo-router';
+import React, { useState } from "react";
+import {
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@firebaseConfig"; // Import firebase configuration
+import { useRouter } from "expo-router";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleLogin = async () => {
     if (!email || !password) {
-      Alert.alert('Error', 'Please enter both email and password');
+      Alert.alert("Error", "Please enter both email and password");
       return;
     }
 
     setLoading(true);
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      Alert.alert('Success', 'Login Successful');
-      router.replace('/Dashboard');
+      Alert.alert("Success", "Login Successful");
+      router.replace("/");
     } catch (error) {
-      Alert.alert('Error', 'Login failed');
+      Alert.alert("Error", "Login failed");
     } finally {
       setLoading(false);
     }
@@ -46,8 +52,14 @@ const LoginScreen = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <TouchableOpacity onPress={handleLogin} style={styles.button} disabled={loading}>
-        <Text style={styles.buttonText}>{loading ? 'Signing in...' : 'Sign In'}</Text>
+      <TouchableOpacity
+        onPress={handleLogin}
+        style={styles.button}
+        disabled={loading}
+      >
+        <Text style={styles.buttonText}>
+          {loading ? "Signing in..." : "Sign In"}
+        </Text>
       </TouchableOpacity>
     </>
   );
@@ -55,21 +67,21 @@ const LoginScreen = () => {
 
 const styles = StyleSheet.create({
   input: {
-    width: '80%',
+    width: "80%",
     height: 40,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     marginBottom: 15,
     paddingLeft: 10,
   },
   button: {
-    backgroundColor: '#007AFF',
+    backgroundColor: "#007AFF",
     paddingVertical: 12,
-    width: '80%',
-    alignItems: 'center',
+    width: "80%",
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
   },
 });
 
